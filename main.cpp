506a2a54e1384b9a5ddd9609a2af01b603121cf2
round-thing-over-by-that-square <lewandowski.alex@gmail.com>
2019-01-28 20:49:27 -0900

    add git.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    int anInt = 23;
+    for (auto i = 0; i < anInt; i++) {
+        std::cout << "Hello, World." << endl;
+    }
+    return 0;
+}
