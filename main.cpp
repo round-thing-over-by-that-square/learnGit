8a20ecc9ccd67ace9b3942b8af1ac9881aca5a19
round-thing-over-by-that-square <lewandowski.alex@gmail.com>
2019-01-28 20:56:21 -0900

    add main.cpp with Howdy, Planet!

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@ using std::cout;
 using std::endl;
 
 int main() {
-    int anInt = 23;
+    int anInt = 12;
     for (auto i = 0; i < anInt; i++) {
-        std::cout << "Hello, World." << endl;
+        std::cout << "Howdy, Planet!" << endl;
     }
     return 0;
 }
